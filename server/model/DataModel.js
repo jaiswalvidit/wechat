a59cf35db62c2d90200d24ccfb2906c33a21bba9
Vidit Jaiswal <jaiswalvidit23@gmail.com>
2024-04-10 02:51:00 +0530

    frontend

--- server/model/DataModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dataSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String,
-});
-const DataModel = mongoose.model('Data', dataSchema);
-
-module.exports = DataModel;
